c866c4c89991cc03bff1b1db65e81c3147966f9f
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-05-22 07:22:43 +0900

    adjust chat_id for newgroup

--- telebot_check.py
+++ telebot_check.py
@@ -5,7 +5,7 @@ import telepot
 
 
 
-chat_id = '-357408712'
+chat_id = '-399667322'
 my_token = '1210478577:AAFZTeyvx3DJU4b6WFzg9J6IhxnA4myEmrQ'
 bot = telepot.Bot(my_token)
 bot.sendMessage(chat_id, 'check')
